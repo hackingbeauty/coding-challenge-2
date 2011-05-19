cc40fd1dee1c4b3639cd0aab7a6f45a0f7b4f049
Mark Muskardin <markmuskardin@gmail.com>
2011-05-18 23:38:37 -0700

    successfully iterating over nested hashes in restaurant categories hash

--- monkey_inferno.rb
+++ monkey_inferno.rb
@@ -18,9 +18,34 @@ get '/' do
   consumer = OAuth::Consumer.new(settings.yelp_consumer_key, settings.yelp_consumer_secret, {:site => "http://#{settings.yelp_api_host}"})
   access_token = OAuth::AccessToken.new(consumer, settings.yelp_token, settings.yelp_token_secret)
   path = "/v2/search?term=restaurants&location=san%20francisco"
-  hash = JSON.parse(access_token.get(path).body) #convert JSON to ruby Hash
-  @top_restaurants = hash.fetch("businesses")
-  @restaurant_categories = ""
+  # hash = JSON.parse(access_token.get(path).body) #convert JSON to ruby Hash
+  hash = JSON.parse(settings.yelp_json_response)
+  @restaurants = hash.fetch("businesses")
+  @categories = {}
+  @restaurants.each do |restaurant|
+    category = restaurant["categories"][0][0]
+    arr = []
+    arr << restaurant
+    h = {}
+    if @categories.has_key?(category)
+      previous_category = @categories[category][0]
+      # puts "======"
+      # p previous_category
+      arr << previous_category
+    end
+    h["restaurants"] = arr      
+    @categories[category] = arr
+  end
+
+  # hash.each{|k,(a,b)| p a,b}
+  
+  @categories.each do |cat|
+     puts "===="
+        p cat
+   end
+  
+  # p @restaurants
+
   @most_recent_reviews = ""
   haml :index
 end
