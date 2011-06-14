8a0e3460678e3c98c17e03534eb0cd3480deb866
Mark Muskardin <markmuskardin@gmail.com>
2011-06-14 12:50:55 -0700

    changes

--- monkey_inferno.rb
+++ monkey_inferno.rb
@@ -5,6 +5,7 @@ require 'yaml'
 require 'json'
 require 'oauth'
 require 'json'
+require 'open-uri'
 
 ### Configuration
 configure do   
@@ -14,12 +15,18 @@ configure do
   end
 end
 
+# get '/heroku' do
+#   puts "=================="
+#   make_api_call('http://livingroom/_design/sessions/_view/videosession')
+#   puts "made make_api_call"
+# end
+
 get '/' do
   consumer = OAuth::Consumer.new(settings.yelp_consumer_key, settings.yelp_consumer_secret, {:site => "http://#{settings.yelp_api_host}"})
   access_token = OAuth::AccessToken.new(consumer, settings.yelp_token, settings.yelp_token_secret)
   path = "/v2/search?term=restaurants&location=san%20francisco"
-  hash_of_restaurants = JSON.parse(access_token.get(path).body) #convert JSON to ruby Hash
-  # hash_of_restaurants = JSON.parse(settings.yelp_json_response)
+  # hash_of_restaurants = JSON.parse(access_token.get(path).body) #convert JSON to ruby Hash
+  hash_of_restaurants = JSON.parse(settings.yelp_json_response)
   hash_of_reviews  = JSON.parse(settings.yelp_json_response_for_reviews)
   @restaurants = hash_of_restaurants.fetch("businesses")
   @categories = get_categories(@restaurants)
@@ -27,20 +34,33 @@ get '/' do
   haml :index
 end
 
+private
+
+  def make_api_call(request)
+    response = ''
+    open(request) do |f|
+      f.each do |line|
+        response = response + line
+      end
+    end
+    return response
+  end
+
 def get_categories(restaurants)
+  puts "====="
+  # p restaurants
   categories = {}
   restaurants.each do |restaurant|
     category = restaurant["categories"][0][0]
     arr = []
-    arr << restaurant
-    h = {}
     if categories.has_key?(category)
-      previous_category = categories[category][0]
-      arr << previous_category
+      stored_restaurant = categories[category][0]
+      arr << stored_restaurant
     end
-    h["restaurants"] = arr      
+    arr << restaurant
     categories[category] = arr
   end
+  p categories
   return categories
 end
 
